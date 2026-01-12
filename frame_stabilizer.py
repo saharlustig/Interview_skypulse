f8ec65419f37c41cb3f6e9e1a169926e8748e42f
SaharL <saharlustig@gmail.com>
2026-01-12 18:17:01 +0200

    feat(FrameStabilizer): cleaning code

--- frame_stabilizer.py
+++ frame_stabilizer.py
@@ -9,35 +9,20 @@ class FrameStabilizer:
         self.height = height
         self.buffer_size = buffer_size
 
-        self.prev_smooth = np.zeros(3, dtype=np.float64)
+        self.prev_smooth_trajectory = np.zeros(3, dtype=np.float64)
         self.frame_buffer = deque(maxlen=self.buffer_size)
-        self.gray_buffer = deque(maxlen=self.buffer_size)
-        self.transform_buffer = deque(maxlen=self.buffer_size)
-        self.trajectory_buffer = deque(maxlen=self.buffer_size)
-        self.cumulative_trajectory = np.zeros(3, dtype=np.float64)
-
-    @property
-    def middle_buffer_frame(self):
-        return self.buffer_size // 2
+        self.grayscale_buffer = deque(maxlen=self.buffer_size)
+        self.cumulative_motion_buffer = deque(maxlen=self.buffer_size)
+        self.cumulative_motion = np.zeros(3, dtype=np.float64)
 
     def stabilize(self, frame):
         self.append_raw_frame(frame)
         if len(self.frame_buffer) > 1: self.estimate_transform()
-        # if len(self.frame_buffer) == self.buffer_size:
         smoothed_frame = self.smooth_frame()
         return smoothed_frame
-        # return frame
 
     def smooth_frame(self):
-        # only output when buffer is full
-        # smooth trajectory
-        smooth_traj = self.smooth_trajectory()
-
-        # center frame trajectory
-        center_traj = self.cumulative_trajectory
-
-        # correction = smoothed - original
-        correction = smooth_traj - center_traj
+        correction = self.smooth_trajectory() - self.cumulative_motion
         dx_c, dy_c, da_c = correction
 
         M = self.build_affine(dx_c, dy_c, da_c)
@@ -48,14 +33,14 @@ class FrameStabilizer:
 
     def append_raw_frame(self, frame):
         self.frame_buffer.append(frame)
-        self.gray_buffer.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+        self.grayscale_buffer.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
 
     def estimate_transform(self):
-        prev_pts = cv2.goodFeaturesToTrack(self.gray_buffer[-2], 200, 0.01, 30)
+        prev_pts = cv2.goodFeaturesToTrack(self.grayscale_buffer[-2], 200, 0.01, 30)
         if prev_pts is None:
             return 0, 0, 0
 
-        curr_pts, status, _ = cv2.calcOpticalFlowPyrLK(self.gray_buffer[-2], self.gray_buffer[-1], prev_pts, None)
+        curr_pts, status, _ = cv2.calcOpticalFlowPyrLK(self.grayscale_buffer[-2], self.grayscale_buffer[-1], prev_pts, None)
         status = status.reshape(-1)
 
         prev_pts = prev_pts[status == 1]
@@ -72,17 +57,17 @@ class FrameStabilizer:
         dy = m[1, 2]
         da = np.arctan2(m[1, 0], m[0, 0])
 
-        self.cumulative_trajectory += np.array([dx, dy, da])
-        self.trajectory_buffer.append(self.cumulative_trajectory.copy())
+        self.cumulative_motion += np.array([dx, dy, da])
+        self.cumulative_motion_buffer.append(self.cumulative_motion.copy())
 
 
     def make_grayscale(self, frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     def smooth_trajectory(self, alpha=0.02):
-        current = self.cumulative_trajectory
-        smooth = alpha * current + (1 - alpha) * self.prev_smooth
-        self.prev_smooth = smooth
+        current = self.cumulative_motion
+        smooth = alpha * current + (1 - alpha) * self.prev_smooth_trajectory
+        self.prev_smooth_trajectory = smooth
         return smooth
 
     @staticmethod
